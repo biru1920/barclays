109f8b719c91ceb252d11b8994ebf7b2e77b7010
Bir.Kumar <Bir.Kumar@multichoice.co.za>
2021-10-14 18:00:13 +0530

    chaneges added

---
+++ FileData/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ThirdPartyTools;
+
+namespace FileData
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Printdetails(args);
+
+        }
+
+        public static void Printdetails(string[] a)
+        {
+            try
+            {
+                List<string> vpram = new List<string>
+                {
+                    "-v",
+                    "--v",
+                    "/v",
+                    "--version"
+                };
+                List<string> spram = new List<string>
+                {
+                    "-s",
+                    "--s",
+                    "/s",
+                    "--size"
+                };
+
+                if (a[0] == "-v")
+                {
+                    FileDetails fileDetails = new FileDetails();
+                    Console.WriteLine(fileDetails.Version(a[1]));
+                }
+
+                if (vpram.Contains(a[0]))
+                {
+                    FileDetails fileDetails = new FileDetails();
+                    Console.WriteLine(fileDetails.Version(a[0]));
+                }
+                else if (spram.Contains(a[0]))
+                {
+                    FileDetails fileDetails = new FileDetails();
+                    Console.WriteLine(fileDetails.Size(a[0]));
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+        }
+        
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileData;
+using ThirdPartyTools;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory(TestCategory.UnitTest)]
+        public void Version_Success()
+        {
+            string[] a = { "-v" , "C:/test.txt" };
+             
+            FileDetails fileDetails = new FileDetails();
+           var b = fileDetails.Version(a[0]);
+           Assert.AreNotEqual("-v",b);
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategory.UnitTest)]
+        public void Version_Fail()
+        {
+            string[] a = { "-v", "C:/test.txt" };
+
+            FileDetails fileDetails = new FileDetails();
+            var b = fileDetails.Version(a[0]);
+            Assert.AreNotEqual("-c", b);
+            
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategory.UnitTest)]
+        public void Size_Success()
+        {
+            string[] a = { "-v", "C:/test.txt" };
+
+            FileDetails fileDetails = new FileDetails();
+            var b = fileDetails.Size(a[1]);
+            Assert.AreNotEqual("-v", b);
+        }
+
+        [TestMethod]
+        [TestCategory(TestCategory.UnitTest)]
+        public void Size_Fail()
+        {
+            string[] a = { "-v", "C:/test.txt" };
+
+            FileDetails fileDetails = new FileDetails();
+            var b = fileDetails.Size(a[1]);
+            Assert.AreNotEqual("-c", b);
+
+        }
+    }
+}
